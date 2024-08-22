e3756ca33ebcd610252b5a98a81447e6ed578ec1
victoria01reis <vreis8321@gmail.com>
2024-08-22 11:54:13 -0300

    atividadeavaliativa

---
+++ atividadeavaliativa.c
@@ -0,0 +1,64 @@
+Questões Avaliativas
+Utilizar PC e entrega individual
+Utilizar GitHub o Google Scla de aula
+
+1-Qual biblioteca é necessario incluir com diretiva # include para utilizar as funçãos de entrada padrão da linguagem C?
+R= É necessario a biblioteca #include<stdio.h>
+
+
+
+2-O trecho de código realiza a leitura de um número inteiro. Entretanto, ele contém dois erros. Corrija-os e justifique sua resposta.
+
+
+      int n;
+
+      scanf ("%f",n); // o erro se da pela estrutura de dado, sendo %d em vez de %f e também o erro é pela falta do "&" depois da virgula na variavel "n"
+
+
+
+
+
+3-Explique o trecho de código abaixo (linha 2)
+      char C;  // identificado variavel de tipo char "C"
+      scanf("%c", de C); // lendo variavel de tipo char "C"
+      while(getchar()!='\n'); // enquanto não for "\n" ela irá encerrar o loop.
+
+
+
+
+
+4-Explique o trecho de código abaixo
+
+      float largura, comprimento; //identificado variavel de tipo float (largura),(comprimento).
+      scanf ("%f%f", &largura, &comprimento); //lendo variaveis de tipo float (largura),(comprimento).
+
+
+
+
+
+
+5-Explique o trecho de código abaixo
+
+      char nome [80]; // identificando variavel de tipo char "nome" de tamanho 80 caracteres
+      scanf ("%s", &nome); // lendo variavel de tipo char "nome".
+
+
+
+
+
+6-explique o trecho de código abaixo
+
+      printf ("Ola, mundo!"); // printado na tela "Ola, mundo!".
+
+
+
+7-Explique o trecho de código abaixo. Dica a resposta não é a mesma da questão anterior
+
+      printf ("Ola, mundo!\n"); // printado na tela "Ola, mundo!" e indo para a proxima linha com \n.
+
+
+
+
+8-Explique o trecho de código abaixo
+
+      printf ("1+1\n"); //  printado na tela "1+1" e indo para a proxima linha com \n.
